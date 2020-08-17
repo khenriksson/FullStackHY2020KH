632d6ff02a909d422fa03258347ef65593cdf038
Kasper Henriksson <kasper.henriksson@aalto.fi>
2020-08-17 14:15:43 +0300

    4.5 done

--- part4/backend/utils/list_helper.js
+++ part4/backend/utils/list_helper.js
@@ -10,7 +10,24 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (prev, current) => {
+    console.log(prev, 'prev')
+    console.log(current, 'current')
+    return prev.likes > current.likes ? {
+        {
+           title: prev.title,
+           author: prev.author,
+           likes: prev.likes
+        }
+    }, : current
+  }
+
+  return blogs.reduce(reducer)
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
