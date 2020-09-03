0213381d96feefff0633f00e3aad14cb9e825ce7
Kasper Henriksson <kasper.henriksson@aalto.fi>
2020-09-03 21:54:21 +0300

    5.8 done, don't understand why togglable couldn't be used

--- part5/frontend/src/components/Blog.js
+++ part5/frontend/src/components/Blog.js
@@ -1,8 +1,23 @@
 import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import Togglable from './Togglable'
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author}
+      <Togglable buttonLabel='view'>
+        <p> {blog.url}</p>
+        likes {blog.likes} <button>like</button>
+        <p> {blog.user.name}</p>
+      </Togglable>
+    </div>
+  )
+}
 
 export default Blog
