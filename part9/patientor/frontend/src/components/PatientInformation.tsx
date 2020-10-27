d7b8bab960bcedf56896045d0362d84876902318
Kasper Henriksson <kasper.henriksson@aalto.fi>
2020-10-27 19:54:44 +0200

    Fetching one patient

--- part9/patientor/frontend/src/components/PatientInformation.tsx
+++ part9/patientor/frontend/src/components/PatientInformation.tsx
@@ -7,36 +7,39 @@ import { useStateValue } from '../state';
 
 const PatientInformation: React.FC = () => {
   const params: IdPatient = useParams();
-  const [{ patients }, dispatch] = useStateValue();
-  console.log('patients :>> ', patients);
-  console.log('patient[params.id] :>> ', patients[params.id]);
-  const myPatient = patients[params.id];
+  const [{ patient }, dispatch] = useStateValue();
+  //   console.log('patients :>> ', patients);
+  //   console.log('patient[params.id] :>> ', patients[params.id]);
 
   //   let result = params.map((a) => a.id);
 
   //   const [, dispatch] = useStateValue();
+  const id = params.id;
 
-  //   const myPatient = state//   React.useEffect(() => {
-  //     // axios.get<void>(`${apiBaseUrl}/ping`);
+  React.useEffect(() => {
+    // axios.get<void>(`${apiBaseUrl}/ping`);
 
-  //     const fetchPatient = async () => {
-  //       try {
-  //         const { data: patient } = await axios.get<Patient>(
-  //           `${apiBaseUrl}/${params}`
-  //         );
-  //         dispatch({ type: 'SET_PATIENT', payload: patient });
-  //       } catch (e) {
-  //         console.error(e);
-  //       }
-  //     };
-  //     fetchPatient();
-  //   }, [dispatch]);
-
+    const fetchPatient = async () => {
+      try {
+        const { data: patient } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${params.id}`
+        );
+        console.log('patient :>> ', patient);
+        dispatch({ type: 'SET_PATIENT', payload: patient });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    fetchPatient();
+  }, [dispatch]);
+  const myPatient = patient;
+  console.log('patient :>> ', patient);
   console.log('params.id :>> ', params.id);
+  console.log('patient?.id?.dateOfBirth :>> ', patient[id.toString()]);
   return (
     <div>
-      <p>{myPatient?.dateOfBirth}</p>
-      <p>{myPatient?.name} </p>
+      <p>Name: {patient[id.toString()]?.name} </p>
+      <p>Date of Birth: {patient[id.toString()]?.dateOfBirth}</p>
     </div>
   );
 };
--- part9/patientor/frontend/src/state/state.tsx
+++ part9/patientor/frontend/src/state/state.tsx
@@ -6,10 +6,12 @@ import { Action } from './reducer';
 export type State = {
   //   patients: { [id: string]: Patient | undefined };
   patients: { [id: string]: Patient | undefined };
+  patient: { [id: string]: Patient | undefined };
 };
 
 const initialState: State = {
   patients: {},
+  patient: {},
 };
 
 export const StateContext = createContext<[State, React.Dispatch<Action>]>([
